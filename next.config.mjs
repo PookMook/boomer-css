1e71206f637320c798ce027bea32742445c4cfa0
Arthur Juchereau <PookMook@users.noreply.github.com>
2024-12-14 16:49:42 +0000

    chore: remove linting on build

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,12 @@ const plugin = UnpluginParcelMacros.webpack()
 
 /** @type {import('next').NextConfig} */
 export default {
+	eslint: {
+		ignoreDuringBuilds: true,
+	  },
+	  typescript:{
+		ignoreDuringBuilds: true,
+	  },
 	webpack(config) {
 		config.plugins.push(plugin)
 		return config
