312a2870260d3003e422f34a29c7c538315df195
PookMook <arthur.juchereau@gmail.com>
2026-02-19 14:44:47 -0500

    chore: move to nitro deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { nitro } from 'nitro/vite'
 import viteReact from '@vitejs/plugin-react'
 import UnpluginParcelMacros from 'unplugin-parcel-macros'
 
@@ -11,6 +12,7 @@ export default defineConfig({
   plugins: [
     tsConfigPaths(),
     tanstackStart(),
+    nitro(),
     UnpluginParcelMacros.vite(),
     viteReact(),
   ],
